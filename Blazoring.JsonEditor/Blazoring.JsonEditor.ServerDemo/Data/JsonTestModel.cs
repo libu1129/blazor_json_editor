6bd169136df7903887331cd038a5b2cc2b341651
이장호 <libu1129@naver.com>
2022-05-13 12:16:24 +0900

    blazor serverside 기반 테스트 프로젝트 생성

---
+++ Blazoring.JsonEditor/Blazoring.JsonEditor.ServerDemo/Data/JsonTestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazoring.JsonEditor.ServerDemo.Data {
+    public class JsonTestModel {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string JsonValue { get; set; }
+    }
+}
